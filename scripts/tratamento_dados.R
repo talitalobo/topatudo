aa585397148f7224f700c763ea9db06052352f17
talitabac <talitabac@gmail.com>
2016-08-21 18:08:58 -0300

    sync

--- scripts/tratamento_dados.R
+++ scripts/tratamento_dados.R
@@ -71,9 +71,61 @@ propostasTotal[is.na(propostasTotal)] <- 0
 write.csv2(propostasTotal, file = "propostasTotal.csv", row.names = F)
 
 # Agrupa por tipo de licitacao
-
 agrupaPropostasTipo <- propostas2015 %>%
-  group_by(nu_CPFCNPJ, tp_Licitacao, st_Proposta, nu_Licitacao) %>%
+  group_by(nu_CPFCNPJ, tp_Licitacao, st_Proposta) %>%
   summarise(totalVitorias = sum(n()))
 
-empresa.tipo = propostas
+tipo_modalidade_licitacao <- read.csv("C:/Users/bac/workspace/hack6/topatudo/dados/CSV/tipo_modalidade_licitacao.csv")
+
+agrupaPropostasTipo2 <- left_join(agrupaPropostasTipo, tipo_modalidade_licitacao, by = "tp_Licitacao")
+
+write.csv2(agrupaPropostasTipo2, file = "propostasPorTipo.csv", row.names = F)
+
+
+###############
+
+# Correlaçao de convites 
+
+propostasConvite <- propostas2015 %>% 
+  filter(tp_Licitacao == 3)
+
+propostasConvite$id <- paste0(propostasConvite$cd_UGestora, propostasConvite$nu_Licitacao)
+
+id <- as.data.frame(t(unique(propostasConvite$id)))
+
+library(reshape2)
+
+propostasConvite2 <- melt(propostasConvite)
+w <- dcast(propostasConvite, nu_CPFCNPJ~nu_Licitacao)
+x <- as.matrix(w[,-1])
+x[is.na(x)] <- 0
+v <- x %*% t(x)                                   #the magic matrix 
+diag(v) <- 0                                      #repalce diagonal
+dimnames(v) <- list(w[, 1], w[,1])                #name the dimensions
+v
+
+coocorrencia <- as.data.frame(v)
+
+colnames(coocorrencia)
+
+
+selecetedCoocorrencia <- coocorrencia %>%
+  filter(row.names == 97542585000103)
+
+
+write.csv2(coocorrencia, file = "coocorrencia.csv")
+coocorrencia$"4654716000163"
+
+Cluster <- kmeans(coocorrencia, 5)
+Cluster
+
+
+
+unique(propostasConvite$nu_CPFCNPJ)
+
+
+propostasConviteGroup <- propostasConvite %>% 
+  group_by(nu_CPFCNPJ) %>%
+  summarise(texto = paste(id, collapse = ","))
+
+str(propostasConviteGroup)
