912e258b8661f6d8f36fd1dc11ac93594fc4e9ca
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2016-10-08 22:54:00 -0300

    desativada busca ao bd, realizando carregamento do arquivo

--- server-side/index.js
+++ server-side/index.js
@@ -1,6 +1,24 @@
+/**
+ * @author: Eric Breno
+ */
 var express = require('express');
 var main = require("./src/js/main.js");
+var fs = require('fs');
 var app = express();
+
+var empresas = {};
+
+/**
+ * Carrega o arquivo com o cnpj e as empresas para
+ * as buscas.
+ */
+fs.readFile('cnpjcepOk.csv', 'utf-8', function (err, data) {
+    var tudo = data.split("\r\n");
+    for (var i = 1; i < tudo.length; i++) {
+        var dados = tudo[i].split(",");
+        empresas[dados[0]] = dados[1];
+    }
+});
 
 /**
  * Serve os arquivos estáticos de scripts
@@ -28,7 +46,15 @@ app.get('/api/empresas', function (req, res) {
 app.get('/api/empresa/cnpj', function (req, res) {
     var requisicao = req.query;
     var cnpjBuscado = requisicao["cnpj"];
-    main.buscaEmpresaCnpj(res, cnpjBuscado);
+    // FIXME: Removida consulta ao BD temporariamente,
+    // por enquanto lendo o arquivo e realizando consultas diretamente nele
+    // main.buscaEmpresaCnpj(res, cnpjBuscado);
+    var resultado = empresas[cnpjBuscado];
+    if (typeof (resultado) === 'undefined') {
+        res.status(400).send({ error: "Não foi possível encontrar a empresa." });
+    } else {
+        res.status(200).send({ cnpj: cnpjBuscado, cep: resultado });
+    }
 });
 
 function stringInvalida(campo) {
